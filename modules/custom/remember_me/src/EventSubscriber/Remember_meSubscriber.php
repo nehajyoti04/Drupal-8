01e9eb751a37275c009a22f3db253c3044a57b44
JYOTI BOHRA <jyotibohra@JYOTIs-MacBook-Air.local>
2016-07-20 21:58:38 +0530

    removing uneccessary code

--- modules/custom/remember_me/src/EventSubscriber/Remember_meSubscriber.php
+++ modules/custom/remember_me/src/EventSubscriber/Remember_meSubscriber.php
@@ -8,7 +8,28 @@ use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
+
+use Drupal\Core\Authentication\AuthenticationProviderInterface;
+use Drupal\Core\Session\AccountProxyInterface;
+
+
+
+
+use Drupal\Core\Session\AccountInterface;
+
+
+use Drupal\Core\Session;
+use Symfony\Component\HttpFoundation\Session\Storage\NativeSessionStorage;
+use Drupal\Core\Session\WriteSafeSessionHandler;
+
+use Symfony\Component\HttpFoundation\RequestStack;
+
+
+
 class Remember_meSubscriber implements EventSubscriberInterface {
+
+  protected $started = false;
+
 
   public function addAccessAllowOriginHeaders(FilterResponseEvent $event) {
     $response= $event->getResponse();
@@ -21,10 +42,23 @@ class Remember_meSubscriber implements EventSubscriberInterface {
   public function xyz(GetResponseEvent $event) {
 
     $user = $this->accountProxy;
+
+
+    $account = \Drupal::currentUser();
+//    dpm("account id");
+//    dpm($account->id());
+//
+//    dpm("user");
+//    dpm($user);
+//    dpm("account");
+//    dpm($account);
+//
+//    dpm("user id");
+//    dpm($user->id());
     if($user->id()) {
-
-
-      dpm("hello");
+//
+//
+//      dpm("hello 123");
       $account = \Drupal\user\Entity\User::load(\Drupal::currentUser()->id());
       $remember_me_value = \Drupal::service('user.data')->get('remember_me', $account->id(), 'remember_me_value');
 //      dpm("account");
@@ -39,26 +73,28 @@ class Remember_meSubscriber implements EventSubscriberInterface {
 //      dpm($remember_me_value);
 //      dpm("remember managed");
 //      dpm(\Drupal::state()->get('remember_me_managed', 0));
-//
-//      $remember_me_lifetime_value = \Drupal::state()->get('remember_me_lifetime', 604800);
-//      if ($remember_me_value && \Drupal::state()->get('remember_me_managed', 0) != 0) {
+////
+      $remember_me_lifetime_value = \Drupal::state()->get('remember_me_lifetime', 604800);
+//      dpm("remember me life time value");
+//      dpm($remember_me_lifetime_value);
+      if ($remember_me_value && \Drupal::state()->get('remember_me_managed', 0) != 0) {
 //        // Set lifetime as configured via admin settings.
-//        dpm("inside if");
-//        if ($remember_me_lifetime_value != ini_get('session.cookie_lifetime')) {
-//          dpm("inside if if ");
-//          _remember_me_set_lifetime($remember_me_value);
-//        }
-//      }
-//      elseif (!isset($remember_me_value)) {
-//        dpm("inside else");
-//        // If we have cookie lifetime set already then unset it.
-//        if (0 != ini_get('session.cookie_lifetime')) {
-//          _remember_me_set_lifetime(0);
-//        }
-//      }
-
-
-
+        dpm("inside if");
+        if ($remember_me_lifetime_value != ini_get('session.cookie_lifetime')) {
+          dpm("inside if if ");
+          $this->_remember_me_set_lifetime($remember_me_lifetime_value);
+        }
+      }
+      elseif (!isset($remember_me_value)) {
+        dpm("inside else");
+        // If we have cookie lifetime set already then unset it.
+        if (0 != ini_get('session.cookie_lifetime')) {
+          $this->_remember_me_set_lifetime(0);
+        }
+      }
+//
+//
+//
     }
 //    print '<pre>'; print_r("request"); print '</pre>';
 //    $user = $this->accountProxy;
@@ -75,7 +111,8 @@ class Remember_meSubscriber implements EventSubscriberInterface {
    */
   static function getSubscribedEvents() {
 //    $events[KernelEvents::RESPONSE][] = array('addAccessAllowOriginHeaders');
-    $events[KernelEvents::REQUEST][] = array('xyz');
+//    $events[KernelEvents::REQUEST][] = array('xyz');
+    $events[KernelEvents::RESPONSE][] = array('xyz');
     return $events;
 
 //    print '<pre>'; print_r("account proxy"); print '</pre>';
@@ -87,15 +124,109 @@ class Remember_meSubscriber implements EventSubscriberInterface {
   public function _remember_me_set_lifetime($cookie_lifetime) {
 
 
-    print '<pre>'; print_r("inside helper function"); print '</pre>';
+//    print '<pre>'; print_r("inside helper function"); print '</pre>';
+    dpm("inside helper function");
     // We have session started.
     // Lets close the session, change php cookie lifetime variable, and start
     // session again.
     // Prevent sess_write from persisting the session object at this time,
     // it will happen on shutdown.
+//    dpm("cookie life time ");
+//    dpm($cookie_lifetime);
+
+
+
+//    session_write_close();
+
+//    ini_set('session.cookie_lifetime', $cookie_lifetime);
+//
+////    ini_set('session.cookie_lifetime', 86400);
+//    ini_set('session.gc_maxlifetime', $cookie_lifetime);
+
+
+
+    // Unset the session cookies.
+//    $session_name = $this->getName();
+//    $cookies = $this->requestStack->getCurrentRequest()->cookies;
+//    if ($cookies->has($session_name)) {
+//      $params = session_get_cookie_params();
+//      setcookie($session_name, '', $cookie_lifetime, $params['path'], $params['domain'], $params['secure'], $params['httponly']);
+//
+//    }
+
+
+//    $this->sessionConfiguration;
+
+//    $session_manager = $this->sessionManager;
+
+    $session_manager = \Drupal::service('session_manager');
+//    $session_manager->save(FALSE);
+    dpm("session manager");
+//    session_write_close();
+//    $session_manager->save(TRUE);
+    ini_set('session.cookie_lifetime', $cookie_lifetime);
+    $session_manager->setOptions(array('cookie_lifetime' => $cookie_lifetime));
+
+//    $session_manager->setSaveHandler(FALSE);
+
+//    $session_manager->start();
+//    dpm("session manager");
+//    dpm($session_manager);
+
+    $session = \Drupal::service('session');
+//    $session->restart();
+//     dpm("session");
+//     dpm($session);
+
+
+//    $session_manager->sessionHandler->isSessionWritable(), FALSE);
+
+//    ->sessionHandler->isSessionWritable(), TRUE);
+
+//    $session_manager->start();
+
+
+    // Force-start a session.
+    $session_manager->start();
+// Check whether a session has been started.
+    $session_manager->isStarted();
+// Migrate the current session from anonymous to authenticated (or vice-versa).
+    $session_manager->regenerate();
+//    drupal_session_initialize();
+
+
+
+
+
+
+//    $session = \Drupal::service('session');
+//    // Alternatively
+//    $session = $request->getSession();
+//
+//    // Force-start a session.
+//    $session->start();
+//    // Check whether a session has been started.
+//    $session->isStarted();
+//    // Migrate the current session from anonymous to authenticated (or vice-versa).
+//    $session->migrate();
+
+    // $session = \Drupal::service('session');
+    // $session->save(FALSE);
+    // session_write_close();
+    // $session->save(TRUE);
+
+    // ini_set('session.cookie_lifetime', $cookie_lifetime);
+
+
+
+//     $session_manager = Drupal::service('session_manager');
+//     dpm("session manager");
+//     dpm($session_manager);
+
 //    drupal_save_session(FALSE);
 //    session_write_close();
 //    drupal_save_session(TRUE);
+//
 //
 //    ini_set('session.cookie_lifetime', $cookie_lifetime);
 //
@@ -103,10 +234,22 @@ class Remember_meSubscriber implements EventSubscriberInterface {
 //    drupal_session_initialize();
   }
 
-  public function __construct() {
-    $user = \Drupal::currentUser();
-//    print '<pre>'; print_r("account proxy"); print '</pre>';
-    $this->accountProxy = $user;
+//  public function __construct() {
+//    $user = \Drupal::currentUser();
+////    print '<pre>'; print_r("account proxy"); print '</pre>';
+//    $this->accountProxy = $user;
+//  }
+
+//  public function __construct(AccountProxyInterface $account_proxy) {
+//    $this->accountProxy = $account_proxy;
+//  }
+
+  public function __construct(AuthenticationProviderInterface $authentication_provider, AccountProxyInterface $account_proxy) {
+    $this->authenticationProvider = $authentication_provider;
+//    $this->filter = ($authentication_provider instanceof AuthenticationProviderFilterInterface) ? $authentication_provider : NULL;
+//    $this->challengeProvider = ($authentication_provider instanceof AuthenticationProviderChallengeInterface) ? $authentication_provider : NULL;
+    $this->accountProxy = $account_proxy;
+
   }
 
 }
