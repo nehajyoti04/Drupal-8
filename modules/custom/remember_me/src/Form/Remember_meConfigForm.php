a925a347a30eb6dcee3647d888432a3c6552d0a8
JYOTI BOHRA <nehajyoti04@gmail.com>
2016-07-21 17:57:52 +0530

    Adding correct function for format interval option array

--- modules/custom/remember_me/src/Form/Remember_meConfigForm.php
+++ modules/custom/remember_me/src/Form/Remember_meConfigForm.php
@@ -13,6 +13,9 @@ use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
 //use Symfony\Component\HttpFoundation\Request;
 
+
+
+
 class Remember_meConfigForm extends ConfigFormBase {
   public function getFormId() {
     return 'remember_me_admin_settings';
@@ -30,11 +33,11 @@ class Remember_meConfigForm extends ConfigFormBase {
     global $user;
 
     $time_intervals = array(30, 3600, 10800, 21600, 43200, 86400, 172800, 259200, 604800, 1209600, 2592000, 5184000, 7776000);
-
-
-
-
-   $options =  array_map(array(\Drupal::service('date.formatter'), 'formatInterval'), array_combine($time_intervals, $time_intervals));
+//
+//
+//
+//
+//   $options =  array_map(array(\Drupal::service('date.formatter'), 'formatInterval'), array_combine($time_intervals, $time_intervals));
 
 
 //    $options = array_combine($time_intervals, $time_intervals);
@@ -47,6 +50,20 @@ class Remember_meConfigForm extends ConfigFormBase {
 
 
 
+
+//    $time_intervals = [30, 3600, 10800, 21600, 43200, 86400, 172800, 259200, 604800, 1209600, 2592000, 5184000, 7776000];
+
+    $options = $this->build_options($time_intervals);
+
+//    function build_options(array $time_intervals, $granularity = 2, $langcode = NULL) {
+//      $callback = function ($value) use ($granularity, $langcode) {
+//        return \Drupal::service('date.formatter')->formatInterval($value, $granularity, $langcode);
+//      };
+//
+//      return array_combine($time_intervals, array_map($callback, $time_intervals));
+//    }
+
+//    dpm("Test run duration: " . \Drupal::service('date.formatter')->formatInterval(30 ));
 
 
 
@@ -71,24 +88,30 @@ class Remember_meConfigForm extends ConfigFormBase {
 
 
 
-    $vars = array(
-      'remember' => array(
-        '#type' => 'item',
-        '#title' => t('Remember me'),
-//        '#value' => isset($user->data['remember_me']) ? t('Yes') : t('No'),
-        '#value' => t('Yess'),
-        '#description' => t('Current user chose at log in.'),
-      ),
-      'session' => array(
-        '#type' => 'item',
-        '#title' => t('Session lifetime'),
-//        '#value' => format_interval(ini_get('session.cookie_lifetime')),
-        '#description' => t('Currently configured session cookie lifetime.'),
-      ),
-    );
-    $form['legend'] = array(
-      '#type' => 'markup',
-//      '#markup' => theme('remember_me_settings_display', array('vars' => $vars)),
+//    $vars = array(
+//      'remember' => array(
+//        '#type' => 'markup',
+//        '#markup' => t('Yess'),
+//        '#title' => t('Remember me'),
+////        '#value' => isset($user->data['remember_me']) ? t('Yes') : t('No'),
+////        '#value' => t('Yess'),
+//        '#description' => t('Current user chose at log in.'),
+//      ),
+//      'session' => array(
+//        '#type' => 'item',
+//        '#title' => t('Session lifetime'),
+//        '#value' => 30,
+////        '#value' => format_interval(ini_get('session.cookie_lifetime')),
+//        '#description' => t('Currently configured session cookie lifetime.'),
+//      ),
+//    );
+//
+
+    $form['session'] = array(
+      '#type' => 'item',
+      '#markup' => t("yes"),
+      '#title' => t('Session lifetime'),
+      '#description' => t('Currently configured session cookie lifetime.'),
     );
 
     $form['remember_me_managed'] = array(
@@ -143,30 +166,39 @@ class Remember_meConfigForm extends ConfigFormBase {
     parent::submitForm($form, $form_state);
   }
 
-  public function format_interval($interval, $granularity = 2, $langcode = NULL) {
-    $units = array(
-      '1 year|@count years' => 31536000,
-      '1 month|@count months' => 2592000,
-      '1 week|@count weeks' => 604800,
-      '1 day|@count days' => 86400,
-      '1 hour|@count hours' => 3600,
-      '1 min|@count min' => 60,
-      '1 sec|@count sec' => 1,
-    );
-    $output = '';
-    foreach ($units as $key => $value) {
-      $key = explode('|', $key);
-      if ($interval >= $value) {
-        $output .= ($output ? ' ' : '') . format_plural(floor($interval / $value), $key[0], $key[1], array(), array('langcode' => $langcode));
-        $interval %= $value;
-        $granularity--;
-      }
 
-      if ($granularity == 0) {
-        break;
-      }
+      function build_options(array $time_intervals, $granularity = 2, $langcode = NULL) {
+      $callback = function ($value) use ($granularity, $langcode) {
+        return \Drupal::service('date.formatter')->formatInterval($value, $granularity, $langcode);
+      };
+
+      return array_combine($time_intervals, array_map($callback, $time_intervals));
     }
-    return $output ? $output : t('0 sec', array(), array('langcode' => $langcode));
-  }
+
+//  public function format_interval($interval, $granularity = 2, $langcode = NULL) {
+//    $units = array(
+//      '1 year|@count years' => 31536000,
+//      '1 month|@count months' => 2592000,
+//      '1 week|@count weeks' => 604800,
+//      '1 day|@count days' => 86400,
+//      '1 hour|@count hours' => 3600,
+//      '1 min|@count min' => 60,
+//      '1 sec|@count sec' => 1,
+//    );
+//    $output = '';
+//    foreach ($units as $key => $value) {
+//      $key = explode('|', $key);
+//      if ($interval >= $value) {
+//        $output .= ($output ? ' ' : '') . format_plural(floor($interval / $value), $key[0], $key[1], array(), array('langcode' => $langcode));
+//        $interval %= $value;
+//        $granularity--;
+//      }
+//
+//      if ($granularity == 0) {
+//        break;
+//      }
+//    }
+//    return $output ? $output : t('0 sec', array(), array('langcode' => $langcode));
+//  }
 }
 
